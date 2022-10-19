3e72f7276e21603352974b72b938368508618a2e
jmacey <jmacey@bournemouth.ac.uk>
2022-10-19 18:13:52 +0100

    added more tests for the image class

--- TurtleExamples/BasicTurtle.py
+++ TurtleExamples/BasicTurtle.py
@@ -1,7 +1,45 @@
 #!/usr/bin/env python
 
-def main() :
-    pass
+import turtle
 
-if __name__ == "__main__" :
+_distance = 10
+
+
+def quit():
+    turtle.bye()
+
+
+def up():
+    turtle.setheading(90)
+    turtle.forward(_distance)
+
+
+def down():
+    turtle.setheading(270)
+    turtle.forward(_distance)
+
+
+def left():
+    turtle.setheading(180)
+    turtle.forward(_distance)
+
+
+def right():
+    turtle.setheading(0)
+    turtle.forward(_distance)
+
+
+def main():
+
+    turtle.listen()
+    turtle.onkey(up, "Up")
+    turtle.onkey(down, "Down")
+    turtle.onkey(left, "Left")
+    turtle.onkey(right, "Right")
+    turtle.onkey(quit, "Escape")
+
+    turtle.mainloop()
+
+
+if __name__ == "__main__":
     main()
